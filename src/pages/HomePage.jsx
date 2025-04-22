15531d57d42c8e88c8379d1b07ef9c528f497d54
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-22 22:29:55 +0200

    feat: home page wip

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,11 @@
 function HomePage() {
     return (
-        <h1>Home page</h1>
+        <div className="home-page p-relative">
+            <div className="lockscreen-logo d-flex flex-column align-center">
+                <img className="logo" src="/img/scg-logo-white.png" alt="" />
+                <p className="subtitle text-uppercase">Graphic Designer - Motion Graphics - Content Creator</p>
+            </div>
+        </div>
     )
 }
 
